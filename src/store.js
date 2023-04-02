cf9133eb279a6a8156a5f205c00a0289ef785d00
nicolo fossati <nicofossa01@gmail.com>
2023-04-02 13:41:22 +0200

    aggiunta bandiere it, uk, es, ja

--- src/store.js
+++ src/store.js
@@ -2,5 +2,14 @@ import { reactive } from 'vue'
 export const store = reactive({
     searchText: "",
     movies: [],
-    series: []
+    series: [],
+    flags: [
+        {
+            flagList: ['it', 'en', 'es', 'ja'],
+            it: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg',
+            en: 'https://upload.wikimedia.org/wikipedia/commons/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg',
+            es: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Spain.svg',
+            ja: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Flag_of_Japan.svg'
+        },
+    ]
 }); 
